1664d8bd6ea10728bb483a2795a2fc036a7b74c5
Chloe Foxglove <chloestout0@gmail.com>
2023-10-24 19:41:52 -0500

    trying out express, node, and threejs together

--- app.js
+++ app.js
@@ -1,72 +1,11 @@
-// A very basic web server in node.js
-// Stolen from: Node.js for Front-End Developers by Garann Means (p. 9-10)
+const express = require('express')
+const app = express()
+const path = require('path')
 
-var port = 8000;
-var serverUrl = "127.0.0.1";
+app.use(express.static(__dirname + '/public'))
+app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')));
+app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')));
 
-var http = require("http");
-var path = require("path");
-var fs = require("fs");
-var checkMimeType = true;
-
-console.log("Starting web server at " + serverUrl + ":" + port);
-
-http.createServer( function(req, res) {
-
-	var now = new Date();
-
-	var filename = req.url || "index.html";
-	var ext = path.extname(filename);
-	var localPath = __dirname;
-	var validExtensions = {
-		".html" : "text/html",
-		".js": "application/javascript",
-		".css": "text/css",
-		".txt": "text/plain",
-		".jpg": "image/jpeg",
-		".gif": "image/gif",
-		".png": "image/png",
-		".woff": "application/font-woff",
-		".woff2": "application/font-woff2"
-	};
-
-	var validMimeType = true;
-	var mimeType = validExtensions[ext];
-	if (checkMimeType) {
-		validMimeType = validExtensions[ext] != undefined;
-	}
-``
-	if (validMimeType) {
-		localPath += filename;
-		fs.exists(localPath, function(exists) {
-			if(exists) {
-				console.log("Serving file: " + localPath);
-				getFile(localPath, res, mimeType);
-			} else {
-				console.log("File not found: " + localPath);
-				res.writeHead(404);
-				res.end();
-			}
-		});
-
-	} else {
-		console.log("Invalid file extension detected: " + ext + " (" + filename + ")")
-	}
-
-}).listen(port, serverUrl);
-
-function getFile(localPath, res, mimeType) {
-	fs.readFile(localPath, function(err, contents) {
-		if(!err) {
-			res.setHeader("Content-Length", contents.length);
-			if (mimeType != undefined) {
-				res.setHeader("Content-Type", mimeType);
-			}
-			res.statusCode = 200;
-			res.end(contents);
-		} else {
-			res.writeHead(500);
-			res.end();
-		}
-	});
-}
+app.listen(3000, () =>
+  console.log('Visit http://127.0.0.1:3000')
+);
